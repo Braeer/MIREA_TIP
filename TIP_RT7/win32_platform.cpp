53782f2e3c1e084884f9d8a81ba4c32f3eaea8b9
Braeer <114247583+Braeer@users.noreply.github.com>
2023-04-19 10:02:03 +0300

    Add files via upload

---
+++ TIP_RT7/win32_platform.cpp
@@ -0,0 +1,47 @@
+#include <windows.h>
+
+bool running = true;
+
+LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	LRESULT result = 0;
+
+	switch (uMsg) {
+		case WM_CLOSE:
+		case WM_DESTROY: {
+			running = false;
+		} break;
+
+		default: {
+			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
+	}
+	return result;
+}
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+	// Create Window Class
+	WNDCLASS window_class = {};
+	window_class.style = CS_HREDRAW | CS_VREDRAW;
+	window_class.lpszClassName = "Game Window Class";
+	window_class.lpfnWndProc = window_callback;
+	
+	// Register Class
+	RegisterClass(&window_class);
+
+	// Create Window
+	HWND window = CreateWindow(window_class.lpszClassName, "Ping - Pong", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+
+	while (running) {
+		// Input
+		MSG message;
+		while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+
+		// Simulate
+
+		// Render
+	}
+
+}
