ab15bb7e32fd38220d188571bcf212cea3bd2237
Braeer <114247583+Braeer@users.noreply.github.com>
2023-04-25 13:57:55 +0300

    Add files via upload

---
+++ TIP_RT8/win32_platform.cpp
@@ -0,0 +1,85 @@
+#include "utils.cpp"
+#include <windows.h>
+
+global_variable bool running = true;
+
+struct Render_State {
+	int height, width;
+	void* memory;
+
+	BITMAPINFO bitmap_info;
+};
+
+global_variable Render_State render_state;
+
+#include "renderer.cpp"
+
+LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	LRESULT result = 0;
+
+	switch (uMsg) {
+		case WM_CLOSE:
+		case WM_DESTROY: {
+			running = false;
+		} break;
+
+		case WM_SIZE: {
+			RECT rect;
+			GetClientRect(hwnd, &rect);
+			render_state.width = rect.right - rect.left;
+			render_state.height = rect.bottom - rect.top;
+
+			int size = render_state.width * render_state.height * sizeof(unsigned int);
+
+			if (render_state.memory) VirtualFree(render_state.memory, 0, MEM_RELEASE);
+			render_state.memory = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+			render_state.bitmap_info.bmiHeader.biSize = sizeof(render_state.bitmap_info.bmiHeader);
+			render_state.bitmap_info.bmiHeader.biWidth = render_state.width;
+			render_state.bitmap_info.bmiHeader.biHeight = render_state.height;
+			render_state.bitmap_info.bmiHeader.biPlanes = 1;
+			render_state.bitmap_info.bmiHeader.biBitCount = 32;
+			render_state.bitmap_info.bmiHeader.biCompression = BI_RGB;
+
+		} break;
+
+		default: {
+			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
+	}
+	return result;
+}
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+	// Create Window Class
+	WNDCLASS window_class = {};
+	window_class.style = CS_HREDRAW | CS_VREDRAW;
+	window_class.lpszClassName = "Game Window Class";
+	window_class.lpfnWndProc = window_callback;
+	
+	// Register Class
+	RegisterClass(&window_class);
+
+	// Create Window
+	HWND window = CreateWindow(window_class.lpszClassName, "My First Game!", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+	HDC hdc = GetDC(window);
+
+	while (running) {
+		// Input
+		MSG message;
+		while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+
+		// Simulate
+		clear_screen(0xff5500);
+		draw_rect(0, 0, 1, 1, 0x00ff22);
+		draw_rect(30, 30, 5, 5, 0xffff22);
+		draw_rect(-20, 20, 8, 3, 0xffff22);
+
+		// Render
+		StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+	}
+
+}
